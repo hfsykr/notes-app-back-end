b39dc5165a523503c10449d7dd3f3d6c1ab0f2ac
Hafizh <74042975+hfsykr@users.noreply.github.com>
2024-09-14 12:39:02 +0700

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -51,7 +51,7 @@ class NotesHandler {
 
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyNoteOwner(id, credentialId);
+    await this._service.verifyNoteAccess(id, credentialId);
 
     const note = await this._service.getNoteById(id);
 
@@ -70,7 +70,7 @@ class NotesHandler {
 
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyNoteOwner(id, credentialId);
+    await this._service.verifyNoteAccess(id, credentialId);
 
     await this._service.editNoteById(id, request.payload);
 
